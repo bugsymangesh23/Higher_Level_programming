5c00e818026a249d4642c37d5c33c9a2cfe7f98c
bugsymangesh23 <emmanuelmangera08@gmail.com>
2024-11-27 10:58:44 +0300

    added execute function

--- Python_Interpreter/byterun.py
+++ Python_Interpreter/byterun.py
@@ -14,3 +14,18 @@ class Interpreter:
         second_num = self.stack.pop()
         total = first_num + second_num
         self.stack.append(total)
+
+# Method to put everything together and execute
+
+def execute_code(self, what_to_execute):
+    instructions = what_to_execute["instructions"]
+    numbers = what_to_execute["numbers"]
+    for each_step in instructions:
+        instruction, argument = each_step
+        if instruction == "LOAD_VALUE":
+            number = numbers[argument]
+            self.LOAD_VALUE(number)
+        elif instruction == "ADD_TWO_VALUES":
+            self.ADD_TWO_VALUES()
+        elif instruction == "PRINT_ANSWER":
+            self.PRINT_ANSWER()
