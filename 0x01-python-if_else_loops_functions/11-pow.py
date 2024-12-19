0996e75e22db5635e5ad716da94440af04d2f978
bugsymangesh23 <emmanuelmangera08@gmail.com>
2024-12-19 22:31:15 +0300

    The power

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''
+Write a function that computes a to the power of b and return the value.
+Prototype: def pow(a, b):
+Returns the value of a ^ b
+You are not allowed to import any module
+'''
+def pow(a, b):
+    return (a ** b)
