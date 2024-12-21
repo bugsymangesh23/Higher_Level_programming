5164540e899762e5cb859a16202a43eda46c81ba
bugsymangesh23 <emmanuelmangera08@gmail.com>
2024-12-21 22:26:52 +0300

    Easy print

---
+++ 0x02-python-import_modules/101-easy_print.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''
+Write a program that prints #pythoniscool, followed by a new line, in the standard output.
+Your program should be maximum 2 lines long
+You are not allowed to use print or eval or open or import sys in your file 101-easy_print.py
+'''
+#!/usr/bin/python3
+__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
