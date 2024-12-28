80f96f2c143829377f648ea39cf64511600476ba
bugsymangesh23 <emmanuelmangera08@gmail.com>
2024-12-28 23:10:18 +0300

    Switch

---
+++ 0x03-python-data_structures/12-switch.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''
+Complete the source code in order to switch value of a and b
+You can find the source code here
+Your code should be inserted where the comment is (line 4)
+Your program should be exactly 5 lines long
+'''
