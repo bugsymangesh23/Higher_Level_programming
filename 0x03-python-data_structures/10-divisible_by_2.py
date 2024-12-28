aeb86b78629841f7149f289451d994c607356d86
bugsymangesh23 <emmanuelmangera08@gmail.com>
2024-12-28 23:07:55 +0300

    Only by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''
+Write a function that finds all multiples of 2 in a list.
+Prototype: def divisible_by_2(my_list=[]):
+Return a new list with True or False, depending on whether the integer at the same position in the original list is a multiple of 2
+The new list should have the same size as the original list
+You are not allowed to import any module
+'''
