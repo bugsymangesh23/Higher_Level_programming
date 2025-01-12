d1302e8b769f5a350537142ae36b9d94138fef82
bugsymangesh23 <emmanuelmangera08@gmail.com>
2025-01-12 22:55:27 +0300

    Added game description

--- The_Big_Book_of_Small_Python_Projects_/0x01-Bagels/app.py
+++ The_Big_Book_of_Small_Python_Projects_/0x01-Bagels/app.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python3
+"""Bagels, by Al Sweigart al@inventwithpython.com
+ A deductive logic game where you must guess a number based on clues.
+ View this code at https://nostarch.com/big-book-small-python-projects
+ A version of this game is featured in the book "Invent Your Own
+ Computer Games with Python" https://nostarch.com/inventwithpython
+ Tags: short, game, puzzle
+"""
 
+import random
+
+NUM_DIGITS = 3
+MAX_GUESSES = 10
+
+def main():
+    print('''Bagels, a deductive logic game.
+     I am thinking of a {}-digit number with no repeated digits.
+Try to guess what it is. Here are some clues:
+When I say: That means:
+Pico One digit is correct but in the wrong position.
+Fermi One digit is correct and in the right position.
+Bagels No digit is correct.For example, if the secret number was 248 and your guess was 843, the
+clues would be Fermi Pico.'''.format(NUM_DIGITS))
